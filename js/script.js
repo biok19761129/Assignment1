a41fb1b887b5203ffe66303da42ba719b3d59a30
Alberto Gomez <biok19761129@yahoo.com>
2016-06-17 16:54:06 -0700

    Master Files

---
+++ js/script.js
@@ -0,0 +1,201 @@
+//STEP 1
+    /*
+    var someMonth;
+    function theMonth(); //function to return current month 
+    currentMonth; //a constant
+    var summerMonth; // and array of summer months
+    MyLibraryAFunction; // a function
+    */
+
+//STEP 2
+    /*
+    4.5
+    "Hello, World"
+    true
+    null
+    */
+//STEP 3
+    /*
+    var myNumber = 10 + (8*5);
+    var myString = "I'm currently" + 39 + " years old";
+    */
+
+//STEP 4
+    /*
+    var firstName, lastName, address, city, state, zipCode, yourAge, referralSource, mayWeContactYou;
+    */
+
+//STEP 5
+    /*
+    var firstName;
+    var lastName;
+    firstName="Alberto";
+    lastName="Gomez";
+    
+    var address="123 Main", city="San Diego", state="CA", zipCode ="98765";
+    
+    var yourAge= 37;
+    var referralSource ="SMC"
+    var mayWeContactYou=false;
+    */
+
+//STEP 6
+    /*
+    var myFirstVar = "I am " + 39 + " years Old";
+    var mySecondVar = "This is my result: " + true;
+    var myThirdVar = 49 + false;
+
+    console.log(typeof(myFirstVar));
+    console.log(typeof(mySecondVar));
+    console.log(typeof(myThirdVar));
+    */
+
+//STEP 7
+    /*
+    console.log ("This variabale is: " + a);        
+    var a = 20; 
+    */
+
+//STEP 8
+    /*
+   var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+   console.log(someString);
+   */
+
+//STEP 9
+    /*
+    var myNullValue=null;
+    console.log(myNullValue);
+    
+    var myNullValue2 = ''; ????
+
+    var myUndefinedVar;
+    console.log(myUndefinedVar);
+    */
+
+//STEP 10
+    /*
+    var myNumber = 4;
+    console.log(typeof(myNumber));
+    
+    var myString = "This is my string";
+    console.log(typeof(myString));
+
+    var myBoolean = true;
+    console.log(typeof(myBoolean));
+
+    var myObject = new Date();
+    console.log(typeof(myObject));
+    
+    var myUndefined;
+    console.log(typeof(myUndefined));
+    */
+
+//STEP 11
+    /*
+    alert("Hello " + "Alberto Gomez" + ", welcome to the JavaScript class!");
+    */
+
+//STEP 12
+    /*
+    var name = "Alberto Gomez";
+    var lastName ="Gomez";
+    alert("Hello " + name + ", welcome to the JavaScript class!");
+    */
+
+//STEP 13
+    /*
+    var course = "JavaScript"
+    var name = "Alberto Gomez";
+    var lastName ="Gomez";
+    alert("Hello " + name + ", welcome to the " + course + " class!");
+    */
+
+//STEP 14
+    /*
+    var course = "JavaScript"
+    var name = "Alberto Gomez";
+    var lastName ="Gomez";
+    alert("Hello " + name + "\n" + "Welcome to the " + course + " class!");
+    */
+    
+//STEP 15
+    /*
+    var course = "JavaScript"
+    var name = prompt("What is your name?");
+    alert("Hello " + name + "\n" + "Welcome to the " + course + " class!");
+    */
+    
+//STEP 16    
+    /*
+    var name = prompt("What is your name?");
+    var course = prompt("And what class are you taking?");
+    alert("Hello " + name + "\n" + "Welcome to the " + course + " class!");
+    */
+
+//STEP 17
+    /*
+    var x = 10;
+    var y = 20;
+    console.log(x + y);
+    */
+
+//STEP 18
+    /*
+    var x = 20;
+    x+= 20;
+    console.log(x);
+    */
+
+//STEP 19
+    /*
+    var x = 20;
+    console.log(x*=5);
+    */
+
+//STEP 20
+    /*
+    var x =20%3;
+    console.log(x);
+    */
+    
+//STEP 21
+    /*
+    x= 5;
+    y= 10;
+    console.log(y === 10 && x === 5);
+    */
+
+//STEP 22
+    /*
+    x= 5;
+    y= 10;
+    console.log(y != 10 || x < 3);
+    */
+
+//STEP 23
+    /*
+    var widget = new Object;
+    console.log(typeof(widget));
+    */
+
+//STEP 24
+    /*
+    var widget = new Object;
+    console.log( widget instanceof(Object));
+    */
+
+//STEP 25
+    /*
+    var widget = new Object;
+    console.log(!widget instanceof(Object));
+    */
+
+
+
+
+
+
+
+
+
